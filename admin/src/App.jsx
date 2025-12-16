1804bff5336ef5756084dd88e9a95051a45e4adb
pranith124 <praneeth2.4k@gmail.com>
2025-12-17 02:30:52 +0530

    auth implemented

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <div>
       <h1>Home Page</h1>
        <SignedOut>
-        <SignInButton />
+        <SignInButton  mode="modal"/>
       </SignedOut>
       <SignedIn>
         <UserButton />
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,15 +2,20 @@ import express from 'express';
 import path from 'path';
 import { ENV } from './config/env.js';
 import { connectDB } from './config/db.js';
-import { clerkMiddleware } from '@clerk/express'
+import { clerkMiddleware } from '@clerk/express';
+import {serve} from 'inngest/express';
+import { functions,inngest} from './config/inngest.js';
 import mongoose from 'mongoose'; // Import the default export/entire package object
-const { Connection } = mongoose;
+
 
 const app = express();
 
 const __dirname = path.resolve();
 
+app.use(express.json());
 app.use(clerkMiddleware());
+
+app.use('/api/inngest', serve({client:inngest,functions:functions}));
 
 app.get('/api/health', (req,res) =>{
     res.status(200).json({message:"Success"});
