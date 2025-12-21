556f5cb1cd845712c2ca2004cd4e607f6eea1cb7
pranith124 <praneeth2.4k@gmail.com>
2025-12-21 17:16:57 +0530

    cart-routes added

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,8 @@ import path from 'path';
 import { ENV } from './config/env.js';
 import { connectDB } from './config/db.js';
 
+import cors from 'cors';
+
 import { clerkMiddleware } from '@clerk/express';
 import {serve} from 'inngest/express';
 import { functions,inngest} from './config/inngest.js';
@@ -12,6 +14,7 @@ import userRoutes from './routes/user.route.js';
 import orderRoutes from './routes/order.route.js';
 import reviewRoutes from './routes/review.route.js';
 import productRoutes from './routes/product.route.js';
+import cartRoutes from './routes/cart.route.js';
 
 
 const app = express();
@@ -20,6 +23,7 @@ const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(clerkMiddleware());
+app.use(cors({origin:ENV.CLIENT_URL,credentials:true}));
 
 app.use('/api/inngest', serve({client:inngest,functions:functions}));
 
@@ -28,6 +32,7 @@ app.use('/api/users',userRoutes);
 app.use('/api/oders',orderRoutes);
 app.use('/api/reviews',reviewRoutes);
 app.use('/api/products',productRoutes);
+app.use('/api/cart',cartRoutes);
 
 app.get('/api/health', (req,res) =>{
     res.status(200).json({message:"Success"});
