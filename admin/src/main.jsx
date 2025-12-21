d36ccddc215193bc66d05a605c8af753ea88ed43
pranith124 <praneeth2.4k@gmail.com>
2025-12-21 23:34:16 +0530

    admin dashboard setup

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -2,10 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 import { ClerkProvider } from '@clerk/clerk-react';
+import { BrowserRouter } from 'react-router';
 
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const queryClient = new QueryClient()
 
 if (!PUBLISHABLE_KEY) {
   throw new Error('Missing Publishable Key')
@@ -13,8 +20,12 @@ if (!PUBLISHABLE_KEY) {
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <BrowserRouter>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <QueryClientProvider client={queryClient}>
           <App />
+       </QueryClientProvider>
     </ClerkProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
---
+++ admin/src/pages/DashBoardPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function DashBoardPage() {
+  return (
+    <div>
+      DashBoard Page
+    </div>
+  )
+}
+
+export default DashBoardPage
