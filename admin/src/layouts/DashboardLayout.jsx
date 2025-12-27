fedfabeddc12b4d01b00931fcdbb53122b3d9419
pranith124 <praneeth2.4k@gmail.com>
2025-12-27 14:24:20 +0530

    dashboard page added

--- admin/src/layouts/DashboardLayout.jsx
+++ admin/src/layouts/DashboardLayout.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import { Outlet } from 'react-router'
+import Navbar from '../components/Navbar'
+import Sidebar from '../components/Sidebar'
 
 function DashboardLayout() {
   return (
-    <div>
-      side bar
-      navbar
-      <Outlet></Outlet>
+    <div className='drawer lg:drawer-open'>
+      <input id="my-drawer"type="checkbox" className='drawer-toggle'  defaultChecked/>
+
+      <div className='drawer-content'>
+        <Navbar/>
+        
+        <main className='p-6'>
+          <Outlet></Outlet>
+        </main>
+        
+      </div>
+      <Sidebar/>
     </div>
   )
 }
